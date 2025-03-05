f9714b254c9d3caed183c2363e460ad35100a549
issac0v0 <gagasalamer@gmail.com>
2025-03-05 21:37:14 +0800

    feat: fix config

--- prepare_puppeteer_env.sh
+++ prepare_puppeteer_env.sh
@@ -1,39 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-# Install puppeteer and its dependencies
-# Skip Chromium download as we'll use Google Chrome later
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Install Puppeteer without downloading Chromium (we'll use Google Chrome instead)
 PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm install puppeteer
 
-# Install Google Chrome 
-# Update the package list
+# Update package list and install required dependencies
 apt-get update \
-    # Install wget and gnupg for key management
     && apt-get install -y wget gnupg \
-    # Add Google's signing key
     && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
-    # Add Google Chrome repository to apt sources
-    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
-    # Update the package list again after adding the new repository
+    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list' \
     && apt-get update \
-    # Install Google Chrome Stable and necessary fonts and libraries
-    # --no-install-recommends skips installing recommended but non-essential packages
-    && apt-get install -y google-chrome-stable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf libxss1 \
-      --no-install-recommends \
-    # Remove the apt cache to save disk space
-    && rm -rf /var/lib/apt/lists/*
+    && apt-get install -y google-chrome-stable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf libxss1 --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*  # Clean up apt cache to save space
 
-# Copy google-chrome-stable to the current directory
 # Find the path of google-chrome-stable
-chrome_path=$(which google-chrome-stable)
+chrome_path=$(which google-chrome-stable || true)
 
-# Check if google-chrome-stable is found
+# Verify if Google Chrome was installed successfully
 if [ -n "$chrome_path" ]; then
     # Move the Chrome executable to the current directory
     mv "$chrome_path" .
     echo "google-chrome-stable moved to current directory."
 else
-    # Print an error message if Chrome is not found
-    echo "not found google-chrome-stable"
-    # Exit the script with an error code
+    echo "Error: google-chrome-stable not found."
     exit 1
 fi
